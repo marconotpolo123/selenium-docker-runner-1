7263a375d30d3bc14e34feef2a0814db940cbf1d
Peng Zhang <pengzhang03091987@gmail.com>
2023-12-08 12:45:05 +1100

    modify docker-compose

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-BROWSER=chrome docker-compose up --scale chrome=2 -d
+docker-compose up --scale chrome=2 -d
 
-BROWSER=firefox docker-compose up --scale firefox=2 -d
+docker-compose up --scale firefox=2 -d
 
 docker-compose down
